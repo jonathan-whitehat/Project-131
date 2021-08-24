a4f2b0b0cc5fe613d0768477f63b93f0138e1b87
jonathan-whitehat <76190639+jonathan-whitehat@users.noreply.github.com>
2021-08-24 19:04:51 -0400

    Add files via upload

---
+++ BOT.js
@@ -0,0 +1,17 @@
+img = "";
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function preload()
+{
+    img = loadImage('BOT.jpg');
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
---
+++ BR.js
@@ -0,0 +1,18 @@
+img = "";
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+}
+
+function preload()
+{
+    img = loadImage('BR.jpg');
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+}
+
